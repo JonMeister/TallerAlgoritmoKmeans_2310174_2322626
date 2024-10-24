9aba8367c68385924a99e7fbbec1f7afb9dcd16c
Jonathan Aristizabal <jonathan.aristizabal@correounivalle.edu.co>
2024-10-24 11:59:08 -0500

    Added Benchmark package

--- src/main/scala/kmedianas2D/package.scala
+++ src/main/scala/kmedianas2D/package.scala
@@ -1,3 +1,8 @@
 package object kmedianas2D {
+  import scala.annotation.tailrec
+  import scala.collection.{Map, Seq}
+  import scala.collection.parallel.CollectionConverters._
+  import scala.util.Random
+  import common._
 
 }
