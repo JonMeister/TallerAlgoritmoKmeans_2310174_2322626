74b02a079cba223103bfd041f776d0ba29666154
Jonathan Aristizabal <jonathan.aristizabal@correounivalle.edu.co>
2024-10-29 01:33:33 -0500

    immutable collections

--- src/main/scala/kmedianas2D/package.scala
+++ src/main/scala/kmedianas2D/package.scala
@@ -2,7 +2,7 @@ import java.util.concurrent.ForkJoinTask
 
 package object kmedianas2D{
   import scala.annotation.tailrec
-  import scala.collection.{Map,Seq}
+  import scala.collection.immutable.{Map,Seq}
   import scala.collection.parallel.CollectionConverters._
   import scala.util.Random
   import common._
